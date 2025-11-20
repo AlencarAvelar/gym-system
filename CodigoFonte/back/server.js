44366a79bae341edabf7c6302936d463a05dc058
Alencar Avelar <alencarhlavelar@gmail.com>
2025-11-20 01:40:06 -0300

    feature: Crud de uma tabela(cadastro de atividade) finalizado

--- CodigoFonte/back/server.js
+++ CodigoFonte/back/server.js
@@ -1,8 +1,10 @@
 const app = require('./src/app');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+// Define a porta, usando 5000 como padrão (ou a que estiver no .env)
+const PORT = process.env.PORT || 3000; 
 
+// O teste de conexão DB acontece no momento em que a aplicação é iniciada
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
   console.log(`📍 Acesse: http://localhost:${PORT}`);
