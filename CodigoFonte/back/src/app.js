abd324932748ca181cfed63d9ee842c47000d92b
Alencar Avelar <alencarhlavelar@gmail.com>
2025-11-27 01:06:00 -0300

    feature: crud agendamentos pronto e funcinal

--- CodigoFonte/back/src/app.js
+++ CodigoFonte/back/src/app.js
@@ -1,31 +1,27 @@
-
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const agendamentoRoutes = require('./routes/agendamentoRoutes');
-
-
-
-
-
 const atividadeRoutes = require('./routes/atividadeRoutes');
+const agendamentoRoutes = require('./routes/agendamentoRoutes'); // ← ADICIONE ESTA LINHA
 
 const app = express();
 
 // Middlewares
-app.use(cors()); // Necessário para permitir requisições do Postman/Front-end
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Rota de teste
 app.get('/', (req, res) => {
-// ...
+  res.json({ 
+    message: 'API Gym System rodando!',
+    version: '1.0.0'
+  });
 });
 
 // Rotas da API
 app.use('/api/atividades', atividadeRoutes);
-app.use('/api/agendamentos', agendamentoRoutes);
+app.use('/api/agendamentos', agendamentoRoutes); // ← ADICIONE ESTA LINHA
 
-// ...
 module.exports = app;
